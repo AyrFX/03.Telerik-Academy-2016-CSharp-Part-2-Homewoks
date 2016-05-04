ea3ae420cc2e32764ed9afe32659c14376b2e74d
AyrFX <oleg@gbg.bg>
2016-05-04 23:04:08 +0300

    Added Tasks 3, 4 and 5 of Arrays Homework

---
+++ 01. Arrays/03. Compare char arrays/CompareCharArrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class CompareCharArrays
+{
+    static void Main()
+    {
+        char[] firstArray = Console.ReadLine().ToCharArray();
+        char[] secondArray = Console.ReadLine().ToCharArray();
+
+        if (firstArray.Length < secondArray.Length)
+        {
+            Console.WriteLine("<");
+            return;
+        }
+        else if (firstArray.Length > secondArray.Length)
+        {
+            Console.WriteLine(">");
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] > secondArray[i])
+                {
+                    Console.WriteLine(">");
+                    return;
+                }
+                else if (firstArray[i] < secondArray[i])
+                {
+                    Console.WriteLine("<");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine("=");
+    }
+}
---
+++ 01. Arrays/04. Maximal sequence/MaximalSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MaximalSequence
+{
+    static void Main()
+    {
+        short arraySize = short.Parse(Console.ReadLine());
+        int[] array = new int[arraySize];
+        for (short i = 0; i < arraySize; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maxSequence = 1, commonMaxSequence = 0; ;
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] == array[i-1])
+            {
+                maxSequence++;
+                commonMaxSequence = Math.Max(maxSequence, commonMaxSequence);
+            }
+            else
+            {
+                maxSequence = 1;
+            }
+        }
+        Console.WriteLine(commonMaxSequence);
+    }
+}
