0577d0a70d79901ff798c74a09d1a51326064899
AyrFX <oleg@gbg.bg>
2016-05-04 22:06:48 +0300

    Added Tasks 1 and 2 of Arrays Homework

---
+++ 01. Arrays/01. Allocate array/AllocateArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class AllocateArray
+{
+    static void Main()
+    {
+        byte arraySize = byte.Parse(Console.ReadLine());
+        int[] array = new int[arraySize];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i * 5;
+            Console.WriteLine(array[i]);
+        }
+    }
+}
---
+++ 01. Arrays/02. Compare arrays/CompareArrays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class CompareArrays
+{
+    static void Main()
+    {
+        byte arraysSizes = byte.Parse(Console.ReadLine());
+
+        int[] firstArray = new int[arraysSizes];
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            firstArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] secondArray = new int[arraysSizes];
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            secondArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        bool equal = true;
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            if (firstArray[i] != secondArray[i])
+            {
+                equal = false;
+                break;
+            }
+        }
+        if (equal)
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not equal");
+        }
+    }
+}
