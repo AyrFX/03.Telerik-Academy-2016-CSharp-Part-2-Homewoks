5929ef381c167bb92b2978fd1fdba63f0ada2e22
AyrFX <oleg@gbg.bg>
2016-05-06 21:34:29 +0300

    Added Task 18 solved with Longest Increasing Subsequence Algorithm

---
+++ 01. Arrays/18. Remove elements from array/RemoveElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+class RemoveElements
+{
+    static void Main()
+    {
+        short arrayLength = short.Parse(Console.ReadLine());
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] subsequences = new int[arrayLength];
+        for (int i = 0; i < subsequences.Length; i++)
+        {
+            subsequences[i] = 1;
+        }
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                
+                if (array[j] <= array[i] && subsequences[i] < subsequences[j] + 1)
+                {
+                    subsequences[i] = subsequences[j] + 1;
+                }
+            }
+        }
+
+
+        Console.WriteLine(array.Length - subsequences.Max());
+    }
+}
