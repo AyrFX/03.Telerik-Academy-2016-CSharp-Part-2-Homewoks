771a83758b3d4fbf4cf5e7f2ba0556537dbb36c2
AyrFX <oleg@gbg.bg>
2016-05-15 20:45:10 +0300

    Added task 1 of Methods Homework

---
+++ 03. Methods/01. Say Hello/SayHallo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class SayHallo
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+
+        PrintGreeting(name);
+    }
+
+    static void PrintGreeting(string name)
+    {
+        Console.WriteLine("Hello, {0}!", name);
+    }
+}
