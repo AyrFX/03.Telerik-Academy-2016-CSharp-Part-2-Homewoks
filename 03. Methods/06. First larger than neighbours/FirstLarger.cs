91359410b7c9dbaa3e5643ded8df9800812f2547
AyrFX <oleg@gbg.bg>
2016-05-15 21:52:31 +0300

    Added task 6 of Methods homework

---
+++ 03. Methods/06. First larger than neighbours/FirstLarger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class FirstLarger
+{
+    static void Main()
+    {
+        int arrayLength = int.Parse(Console.ReadLine());
+        string[] inputArray = Console.ReadLine().Split(' ');
+        int[] array = Array.ConvertAll(inputArray, int.Parse);
+
+        Console.WriteLine(GetFirstLarger(array));
+    }
+
+    static int GetFirstLarger(int[] array)
+    {
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            if (array[i] >= array[i-1] && array[i] >= array[i+1])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
