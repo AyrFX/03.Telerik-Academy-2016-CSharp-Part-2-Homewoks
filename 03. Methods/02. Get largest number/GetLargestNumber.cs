1cd06e111fe9de5710264b54541d98853ee80a2b
AyrFX <oleg@gbg.bg>
2016-05-15 21:03:20 +0300

    Added tasks 2 and 3 of Methods homework

---
+++ 03. Methods/02. Get largest number/GetLargestNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class GetLargestNumber
+{
+    static void Main()
+    {
+        string[] inputNumbers = Console.ReadLine().Split(' ');
+        int[] numbers = Array.ConvertAll(inputNumbers, int.Parse);
+        int maxNumber = GetMax(GetMax(numbers[0], numbers[1]), GetMax(numbers[1], numbers[2]));
+        Console.WriteLine(maxNumber);
+    }
+
+    static int GetMax(int numberA, int numberB)
+    {
+        return Math.Max(numberA, numberB);
+    }
+}
---
+++ 03. Methods/03. English digit/EnglishDigit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class EnglishDigit
+{
+    static void Main()
+    {
+        string inputNumber = Console.ReadLine();
+
+        Console.WriteLine(digitInWord(inputNumber[inputNumber.Length - 1]));
+    }
+
+    static string digitInWord (char digit)
+    {
+        switch (digit)
+        {
+            case '0':
+                {
+                    return "zero";
+                }
+            case '1':
+                {
+                    return "one";
+                }
+            case '2':
+                {
+                    return "two";
+                }
+            case '3':
+                {
+                    return "three";
+                }
+            case '4':
+                {
+                    return "four";
+                }
+            case '5':
+                {
+                    return "five";
+                }
+            case '6':
+                {
+                    return "six";
+                }
+            case '7':
+                {
+                    return "seven";
+                }
+            case '8':
+                {
+                    return "eight";
+                }
+            case '9':
+                {
+                    return "nine";
+                }
+            default:
+                {
+                    return "";
+                }
+        }
+    }
+}
