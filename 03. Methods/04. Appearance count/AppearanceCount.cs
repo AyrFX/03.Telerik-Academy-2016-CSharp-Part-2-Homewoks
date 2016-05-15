e371bee18c0adc14732a6c50b9c9e9ee34f42d21
AyrFX <oleg@gbg.bg>
2016-05-15 21:14:33 +0300

    Added task 4 of Methods Homework

---
+++ 03. Methods/04. Appearance count/AppearanceCount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class AppearanceCount
+{
+    static void Main()
+    {
+        int arrayLength = int.Parse(Console.ReadLine());
+        string[] inpputNumbers = Console.ReadLine().Split(' ');
+        int[] numbers = Array.ConvertAll(inpputNumbers, int.Parse);
+        int numberForSeek = int.Parse(Console.ReadLine());
+        Console.WriteLine(CountAppearances(numbers, numberForSeek));
+    }
+
+    static int CountAppearances(int[] array, int number)
+    {
+        int appearances = 0;
+        foreach (var element in array)
+        {
+            if (element == number)
+            {
+                appearances += 1;
+            }
+        }
+        return appearances;
+    }
+}
