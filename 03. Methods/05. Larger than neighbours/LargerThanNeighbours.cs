44919803980afb1a76762daa1bf3f5ed8346548c
AyrFX <oleg@gbg.bg>
2016-05-15 21:32:43 +0300

    Added taks 5 of Methods homework

---
+++ 03. Methods/05. Larger than neighbours/LargerThanNeighbours.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class LargerThanNeighbours
+{
+    static void Main()
+    {
+        int numbersCount = int.Parse(Console.ReadLine());
+        string[] inputNumbers = Console.ReadLine().Split(' ');
+        int[] numbers = Array.ConvertAll(inputNumbers, int.Parse);
+        int largerCounter = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (IsLarger(numbers, i))
+            {
+                largerCounter++;
+            }
+        }
+
+        Console.WriteLine(largerCounter);
+    }
+
+    static bool IsLarger(int[] array, int elementPosition)
+    {
+        if (elementPosition == 0 || elementPosition == array.Length - 1)
+        {
+            return false;
+        }
+        else
+        {
+            if (array[elementPosition] >= array[elementPosition + 1] && array[elementPosition] >= array[elementPosition - 1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
