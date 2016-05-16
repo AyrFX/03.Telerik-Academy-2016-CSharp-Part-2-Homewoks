a24d059ade18cc4842a40e04678c834706c1de91
AyrFX <oleg@gbg.bg>
2016-05-16 22:38:32 +0300

    Added task 2 of Numeral systems homework

---
+++ 04. Numeral-Systems/02. Binary to decimal/BinToDec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class BinToDec
+{
+    static void Main()
+    {
+        string binaryNumber = Console.ReadLine();
+        long decimalNumber = 0;
+
+        for (int i = binaryNumber.Length - 1; i >= 0; i--)
+        {
+            decimalNumber += (long)(Char.GetNumericValue(binaryNumber[i]) * Math.Pow(2, binaryNumber.Length - i - 1));
+        }
+
+        Console.WriteLine(decimalNumber);
+    }
+}
