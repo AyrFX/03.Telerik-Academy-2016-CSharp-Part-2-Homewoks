9a00a7a2db96a3d11cfcf2ef92ede7faff52f739
AyrFX <oleg@gbg.bg>
2016-05-16 21:25:29 +0300

    Added task 10 of Methods homework

---
+++ 03. Methods/10. N Factorial/NFactorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+class NFactorial
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(Factorial(number));
+    }
+
+    static BigInteger Factorial(int number)
+    {
+        if (number == 0)
+        {
+            return 1;
+        }
+
+        BigInteger result = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
