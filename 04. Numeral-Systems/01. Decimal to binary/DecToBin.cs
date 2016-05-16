09ae4301c7bf5899a79aba427925ad469ea21c7b
AyrFX <oleg@gbg.bg>
2016-05-16 22:20:39 +0300

    Added task 1 of Numeral systems homework

---
+++ 04. Numeral-Systems/01. Decimal to binary/DecToBin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class DecToBin
+{
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine());
+
+        string binary = "";
+        while (number > 0)
+        {
+            binary = number % 2 + binary;
+            number /= 2;
+        }
+        binary.TrimStart('0');
+
+        Console.WriteLine(binary);
+    }
+}
