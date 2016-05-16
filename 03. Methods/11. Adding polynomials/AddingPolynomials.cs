1cf694d75987eb8dcb355deec1cf76da369c0c81
AyrFX <oleg@gbg.bg>
2016-05-16 21:38:15 +0300

    Added task 11 of Methods homework

---
+++ 03. Methods/11. Adding polynomials/AddingPolynomials.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class AddingPolynomials
+{
+    static void Main()
+    {
+        int polyLength = int.Parse(Console.ReadLine());
+        string[] elements = Console.ReadLine().Split(' ');
+        int[] firstPoly = Array.ConvertAll(elements, int.Parse);
+        elements = Console.ReadLine().Split(' ');
+        int[] secondPoly = Array.ConvertAll(elements, int.Parse);
+
+        int[] result = AddPolynomials(firstPoly, secondPoly);
+        Console.WriteLine(string.Join(" ", result));
+    }
+
+    static int[] AddPolynomials(int[] first, int[] second)
+    {
+        int[] result = new int[first.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = first[i] + second[i];
+        }
+        return result;
+    }
+}
