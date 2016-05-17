3d7c91be37e5022480bec30dd826b939ba719f72
AyrFX <oleg@gbg.bg>
2016-05-17 21:47:36 +0300

    Added task 5 of Using classes and objects homework

---
+++ 05. Using-Classes-and-Objects/05. Triangle surface by three sides/TriangleSurfaceBySides.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class TriangleSurfaceBySides
+{
+    static void Main()
+    {
+        double firstSide = double.Parse(Console.ReadLine());
+        double secondSide = double.Parse(Console.ReadLine());
+        double thirdSide = double.Parse(Console.ReadLine());
+
+        double halfPeremeter = (firstSide + secondSide + thirdSide) / 2;
+        double surface = Math.Sqrt(halfPeremeter * (halfPeremeter - firstSide) * (halfPeremeter - secondSide) * (halfPeremeter - thirdSide));
+        Console.WriteLine("{0:F2}", surface);
+    }
+}
