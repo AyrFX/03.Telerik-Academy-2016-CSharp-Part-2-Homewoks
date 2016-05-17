ce4cd8eaf7b466c4155fd58669f3aca26d62d54e
AyrFX <oleg@gbg.bg>
2016-05-17 22:12:19 +0300

    Added task 7 of Using classes and objects homework

---
+++ 05. Using-Classes-and-Objects/07. Workdays/WorkDays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class WorkDays
+{
+    static void Main()
+    {
+        DateTime futureDate = DateTime.Parse(Console.ReadLine());
+        DateTime[] holidays = new DateTime[10];
+
+        DateTime today = DateTime.Today;
+        int workDays = 0;
+        while (today <= futureDate)
+        {
+            if (today.DayOfWeek == DayOfWeek.Monday ||
+                today.DayOfWeek == DayOfWeek.Tuesday ||
+                today.DayOfWeek == DayOfWeek.Wednesday ||
+                today.DayOfWeek == DayOfWeek.Thursday ||
+                today.DayOfWeek == DayOfWeek.Friday)
+            {
+                if (Array.IndexOf(holidays, today) == -1)
+                {
+                    workDays++;
+                }
+            }
+            today.AddDays(1);
+        }
+    }
+}
