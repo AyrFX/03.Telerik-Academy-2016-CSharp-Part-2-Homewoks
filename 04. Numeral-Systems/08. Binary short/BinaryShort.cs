e4a48841924f1b3234dea4856f3ceeede57793f3
AyrFX <oleg@gbg.bg>
2016-05-17 21:08:39 +0300

    Added task 8 of Numeral systems homework

---
+++ 04. Numeral-Systems/08. Binary short/BinaryShort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class BinaryShort
+{
+    static void Main()
+    {
+        short decimalNumber = short.Parse(Console.ReadLine());
+        string binaryNumber = "";
+
+        if (decimalNumber < 0)
+        {
+            decimalNumber++;
+            decimalNumber += short.MaxValue;
+            binaryNumber = "1" + DecToBin(decimalNumber).PadLeft(15, '0');
+        }
+        else
+        {
+            binaryNumber = "0" + DecToBin(decimalNumber).PadLeft(15, '0');
+        }
+        
+        Console.WriteLine(binaryNumber);
+    }
+
+    static string DecToBin(short decimalNumber)
+    {
+        string binaryNumber = "";
+        while (decimalNumber > 0)
+        {
+            binaryNumber = decimalNumber % 2 + binaryNumber;
+            decimalNumber /= 2;
+        }
+        return binaryNumber;
+    }
+}
