2361386cac28b0f697e59747ad75c4dc245aef87
AyrFX <oleg@gbg.bg>
2016-05-17 21:59:40 +0300

    Added task 6 of Using classes and objects homework

---
+++ 05. Using-Classes-and-Objects/06. Triangle surface - sides and an angle/TiangleSurfaceByTwoSidesAndAngle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class TiangleSurfaceByTwoSidesAndAngle
+{
+    static void Main()
+    {
+        double firstSide = double.Parse(Console.ReadLine());
+        double secondSide = double.Parse(Console.ReadLine());
+        double angle = double.Parse(Console.ReadLine());
+
+        double surface = (firstSide * secondSide * Math.Sin(angle * Math.PI / 180)) / 2;
+        Console.WriteLine("{0:F2}", surface);
+    }
+}
