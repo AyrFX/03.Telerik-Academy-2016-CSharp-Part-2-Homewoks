f366755568efd8e656ae8f5ba8cbc5ef82989b4d
AyrFX <oleg@gbg.bg>
2016-05-17 22:18:49 +0300

    Added task 8 of Using classes and objects homework

---
+++ 05. Using-Classes-and-Objects/08. Sum integers/SumIntegers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class SumIntegers
+{
+    static void Main()
+    {
+        string[] inputElements = Console.ReadLine().Split(' ');
+        short[] numbers = Array.ConvertAll(inputElements, short.Parse);
+
+        int sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
