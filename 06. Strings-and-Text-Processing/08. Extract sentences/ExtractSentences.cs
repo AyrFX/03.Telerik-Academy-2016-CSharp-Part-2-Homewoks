f9112cae901153021bfb857daf942b12f8c4d79f
AyrFX <oleg@gbg.bg>
2016-05-19 21:24:34 +0300

    Added task 8 of Strings and Text Processing homework

---
+++ 06. Strings-and-Text-Processing/08. Extract sentences/ExtractSentences.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class ExtractSentences
+{
+    static void Main()
+    {
+        string word = Console.ReadLine();
+        word = " " + word + " ";
+        string text = Console.ReadLine();
+
+        while(text.IndexOf(".") > 0)
+        {
+            int sentenceEndIndex = text.IndexOf(".");
+            string currentSentence = text.Substring(0, sentenceEndIndex + 1);
+            if (currentSentence.IndexOf(word) > 0)
+            {
+                Console.WriteLine(currentSentence);
+            }
+            text = text.Replace(currentSentence, "");
+            text = text.Trim();
+        }
+    }
+}
