a2d92bd4c7bfccf102d83ddf4f83c61438111d34
AyrFX <oleg@gbg.bg>
2016-05-19 22:47:53 +0300

    Added tasks 10 and 12 of Strings and Text Processing homework

---
+++ 06. Strings-and-Text-Processing/10. Unicode characters/UnicodeChars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class UnicodeChars
+{
+    static void Main()
+    {
+        string inputString = Console.ReadLine();
+        string outputString = string.Empty;
+
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            outputString += "\\u" + string.Format("{0:X4}", Convert.ToInt16(inputString[i]));
+        }
+        Console.WriteLine(outputString);
+    }
+}
---
+++ 06. Strings-and-Text-Processing/12. Parse URL/ParseURL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ParseURL
+{
+    static void Main()
+    {
+        string url = Console.ReadLine();
+        string delimiter = "://";
+        int delimiterStart = url.IndexOf(delimiter);
+        string protocol = url.Substring(0, delimiterStart);
+        url = url.Replace(protocol + delimiter, "");
+        delimiter = "/";
+        delimiterStart = url.IndexOf(delimiter);
+        string server = url.Substring(0, delimiterStart);
+        url = url.Replace(server, "");
+        Console.WriteLine("[protocol] = " + protocol);
+        Console.WriteLine("[server] = " + server);
+        Console.WriteLine("[resource] = " + url);
+    }
+}
