8467cec54bcfafa656c540b9237e36626daa6111
AyrFX <oleg@gbg.bg>
2016-05-19 21:11:37 +0300

    Added tasks 5 and 6 of Strings and Text Processing homework

---
+++ 06. Strings-and-Text-Processing/05. Parse tags/ParseTags.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ParseTags
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine();
+        string openTag = "<upcase>";
+        string closeTag = "</upcase>";
+
+        while (inputText.IndexOf(openTag) > 0)
+        {
+            int taggedTextStartIdex = inputText.IndexOf(openTag);
+            int taggedTextEndIndex = inputText.IndexOf(closeTag);
+            int taggedTextLength = (taggedTextEndIndex + closeTag.Length) - taggedTextStartIdex;
+            string taggedText = inputText.Substring(taggedTextStartIdex, taggedTextLength);
+            inputText = inputText.Replace(taggedText, taggedText.ToUpper());
+            inputText = inputText.Replace(openTag.ToUpper(), "");
+            inputText = inputText.Replace(closeTag.ToUpper(), "");
+        }
+
+        Console.WriteLine(inputText);
+    }
+
+}
---
+++ 06. Strings-and-Text-Processing/06. String length/StringLength.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class StringLength
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine();
+        Console.WriteLine(inputText.PadRight(20, '*'));
+    }
+}
