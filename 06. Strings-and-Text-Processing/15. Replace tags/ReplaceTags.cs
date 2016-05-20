3206bf343f0ba3bcbbf79bbe88e1fe1dda7fc948
AyrFX <oleg@gbg.bg>
2016-05-20 07:16:05 +0300

    Added task 15 of Strings and Text Processing homework

---
+++ 06. Strings-and-Text-Processing/15. Replace tags/ReplaceTags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ReplaceTags
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        text = text.Replace("<a href=\"", "[URL=");
+        text = text.Replace("\">", "]");
+        text = text.Replace("</a>", "(/URL)");
+
+        Console.WriteLine(text);
+    }
+}
