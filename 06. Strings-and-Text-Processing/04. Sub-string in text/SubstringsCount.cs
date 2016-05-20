8c95f4b63bc8a8ba7d1beb787de96f11836cfd44
AyrFX <oleg@gbg.bg>
2016-05-20 20:39:45 +0300

    Corrected task 4 of Strings and text processing homework; rewritten, but the problem was in case insensitive search

--- 06. Strings-and-Text-Processing/04. Sub-string in text/SubstringsCount.cs
+++ 06. Strings-and-Text-Processing/04. Sub-string in text/SubstringsCount.cs
@@ -7,7 +7,13 @@ class SubstringsCount
         string pattern = Console.ReadLine();
         string text = Console.ReadLine();
 
-        int occurences = (text.Length - text.Replace(pattern, "").Length) / pattern.Length;
+        //int occurences = (text.Length - text.Replace(pattern, "").Length) / pattern.Length;
+        int occurences = 0;
+        while (text.IndexOf(pattern, StringComparison.CurrentCultureIgnoreCase) > 0)
+        {
+            occurences++;
+            text = text.Remove(text.IndexOf(pattern, StringComparison.CurrentCultureIgnoreCase), pattern.Length);
+        }
         Console.WriteLine(occurences);
     }
 }
