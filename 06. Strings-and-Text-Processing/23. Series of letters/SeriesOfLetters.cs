60423b22db4dd26283027e5109bda26a3e875326
AyrFX <oleg@gbg.bg>
2016-05-20 07:28:07 +0300

    Added task 23 of Strings and Text Processing homework

---
+++ 06. Strings-and-Text-Processing/23. Series of letters/SeriesOfLetters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class SeriesOfLetters
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine();
+        for (int i = inputText.Length - 1; i > 0; i--)
+        {
+            if (inputText[i] == inputText[i-1])
+            {
+                inputText = inputText.Remove(i, 1);
+            }
+        }
+
+        Console.WriteLine(inputText);
+    }
+}
