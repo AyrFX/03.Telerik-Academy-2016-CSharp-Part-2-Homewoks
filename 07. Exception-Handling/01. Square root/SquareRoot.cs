1463ec2871c68d368177153905c54ec6bd61eb3d
AyrFX <oleg@gbg.bg>
2016-05-21 18:25:58 +0300

    Added task 1 of Exception handling homework

--- 07. Exception-Handling/01. Square root/SquareRoot.cs
+++ 07. Exception-Handling/01. Square root/SquareRoot.cs
@@ -4,6 +4,27 @@ class SquareRoot
 {
     static void Main()
     {
-
+        try
+        {
+            double number = double.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                throw new ArgumentException();
+            }
+            Console.WriteLine("{0:F3}", Math.Sqrt(number));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+        
     }
 }
