7294ccfc58c2e1d82ad6a737144c7284e3b8faa8
AyrFX <oleg@gbg.bg>
2016-05-21 20:49:03 +0300

    Added folder structure for Exam preparation, task 1 and task 2 of 05.03.2015 Evening Exam

---
+++ 09. Exam-Preparation/01.Calculation Problem/CalculationProblem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class CalculationProblem
+{
+    static void Main()
+    {
+        string[] inputData = Console.ReadLine().Split(' ');
+        int sum = 0;
+        foreach (string number in inputData)
+        {
+            sum += FromCode(number);
+        }
+
+        Console.WriteLine("{0} = {1}", ToCode(sum), sum);
+    }
+
+    static int FromCode(string codedNumber)
+    {
+        string digits = "abcdefghijklmnopqrstuvw";
+        int result = 0;
+        for (int i = codedNumber.Length - 1; i >= 0; i--)
+        {
+            result += (int)(digits.IndexOf(codedNumber[i]) * Math.Pow(23, codedNumber.Length - i - 1));
+        }
+        return result;
+    }
+
+    static string ToCode(int number)
+    {
+        string digits = "abcdefghijklmnopqrstuvw";
+        int catsBase = 23;
+        int reminder = 0;
+        string result = string.Empty;
+        while (number > 0)
+        {
+            reminder = number % catsBase;
+            number = number / catsBase;
+            result = digits[reminder] + result;
+        }
+        return result;
+    }
+}
---
+++ 09. Exam-Preparation/2. Even Differences/EvenDiffs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+class EvenDiffs
+{
+    static void Main()
+    {
+        string[] sequence = Console.ReadLine().Split(' ');
+        int[] numbers = Array.ConvertAll(sequence, int.Parse);
+
+        int currentIndex = 1;
+        BigInteger differencesSum = 0;
+        uint currentDifference;
+        while (currentIndex < numbers.Length)
+        {
+            currentDifference = (uint)Math.Abs(numbers[currentIndex] - numbers[currentIndex - 1]);
+            if (currentDifference % 2 == 0)
+            {
+                currentIndex += 2;
+            }
+            else
+            {
+                currentIndex++;
+            }
+            if (currentDifference % 2 == 0)
+            {
+                differencesSum += currentDifference;
+            }
+        }
+        Console.WriteLine(differencesSum);
+    }
+}
