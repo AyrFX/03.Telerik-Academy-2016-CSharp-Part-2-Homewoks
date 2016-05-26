4d8ec0054f82a42999a1914634e1a67a5536d415
AyrFX <oleg@gbg.bg>
2016-05-26 23:10:18 +0300

    Added initial code of task 4 of 05.03.2015 evening exam

---
+++ 09. Exam-Preparation/04. Konspiration/Konspiration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Konspiration
+{
+    public enum States { outsideMethod, methodSignature, methodBody}
+    static void Main()
+    {
+        int codeLines = int.Parse(Console.ReadLine());
+        StringBuilder codeInput = new StringBuilder();
+        for (int i = 0; i < codeLines; i++)
+        {
+            codeInput.AppendLine(Console.ReadLine());
+        }
+        string code = codeInput.ToString();
+
+        bool afterStaticKeyword = false;
+        States currentState;
+        for (int i = 0; i < code.Length; i++)
+        {
+            if (code[i] == 's' && code.Remove(0, i).Remove(6) == "tatic")
+            {
+                currentState = States.methodSignature;
+                i += 6;
+            }
+        }
+    }
+}
