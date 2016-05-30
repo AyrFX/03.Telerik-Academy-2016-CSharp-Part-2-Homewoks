80499818a39fa3565606d699b625c42e233f357f
AyrFX <oleg@gbg.bg>
2016-05-30 22:18:23 +0300

    Corrected task 2 of Exceptions handling homework for 100/100

--- 07. Exception-Handling/02. Enter numbers/EnterNumbers.cs
+++ 07. Exception-Handling/02. Enter numbers/EnterNumbers.cs
@@ -13,16 +13,24 @@ class EnterNumbers
             {
                 outputString += " < ";
             }
-            if (outputString == string.Empty)
+            try
             {
-                currentNumber = ReadNumber(1, 100);
+                if (outputString == string.Empty)
+                {
+                    currentNumber = ReadNumber(1, 100);
+                }
+                else
+                {
+                    currentNumber = ReadNumber(lastNumber, 100);
+                }
+                lastNumber = currentNumber;
+                outputString += currentNumber;
             }
-            else
+            catch (Exception)
             {
-                currentNumber = ReadNumber(lastNumber, 100);
+                Console.WriteLine("Exception");
+                return;
             }
-            lastNumber = currentNumber;
-            outputString += currentNumber;
         }
 
         outputString += " < 100";
@@ -32,18 +40,11 @@ class EnterNumbers
     static int ReadNumber(int start, int end)
     {
         int number;
-        try
-        {
-            number = int.Parse(Console.ReadLine());
-        }
-        catch (FormatException ex)
-        {
-            throw new Exception(ex.Message, ex.InnerException);
-        }
+        number = int.Parse(Console.ReadLine());
 
-        if (number < start || number > end)
+        if (number <= start || number >= end)
         {
-            //throw new ArgumentException(string.Format("The number should be between {0} and {1}", start, end));
+            throw new ArgumentException(string.Format("The number should be between {0} and {1}", start, end));
         }
 
         return number;
